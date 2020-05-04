dbcadc6b1c35ad92ef1bb33d08ff0227139d389f
ruto-txt <48617610+ruto-txt@users.noreply.github.com>
2020-05-04 15:55:02 +0900

    クリック動作をコンソール表示テストに変更

--- vreco-project/public/regist.js
+++ vreco-project/public/regist.js
@@ -1,4 +1,9 @@
 document.regist.submit.addEventListener('click',kurikku);
 function kurikku(){
-    alert('クリックしました？')
+    var name = document.regist.name.value;
+    var furigana = document.regist.furigana.value;
+    var url = document.regist.url.value;
+    var hash = document.regist.hash.value;
+
+    console.log(name,furigana,url,hash);
 }
