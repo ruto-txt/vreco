8bb66f2cf5835ba8144d33fb85c3a49bd8f3a3c1
ruto-txt <48617610+ruto-txt@users.noreply.github.com>
2020-05-05 22:05:27 +0900

    formのラジオボタンをjsで追加するための処理 1　html5でのformタグ属性をちょっと書き足してみた 2　questions.jsに追記 3　script js でいろいろテストコード書いたり書かなかったり

--- vreco-project/public/script.js
+++ vreco-project/public/script.js
@@ -22,21 +22,50 @@ function transitionSelType(){
 }
 
 
-function inputsInit(questions){
-    var src = questions;
-    var form = document.getElementById('mainform');//追加先のform
 
-    var divEl = document.createElement('div');
-    divEl.className = 'form-group'
+function inputsInit(questions,qSentence){
+    var form = document.getElementById('mainform');//追加先のformを取得する
+    var liParEl = document.createElement('li');//番号つきリストに加える要素を生成
 
-    var tmpInputEl = document.createElement('input');
-    tmpInputEl.type = 'radio';
-    tmpInputEl.name = 'mental';
-    tmpInputEl.form='mainform';
-tmpInputEl.value='てすとｗ'
+    for (const key in questions) {//設問のループ
+        if (questions.hasOwnProperty(key)) {
+            const tmp_obj = questions[key];
+            
+            // 設問ブロックを包むdivを生成
+            var divEl = document.createElement('div');
+            divEl.className = 'form-group';
+        
+            var divEl = document.createElement('div');
+            //設問の文言をどうにかして仕込む
+            //TODO::文言を読み取って何かの要素に追加するメソッド
 
-    var tmpLabelEl = document.createElement('label');
-    tmpLabelEl.textContent = 'わろたｗｗ'
-    form.appendChild(tmpInputEl);
-    form.appendChild(tmpLabelEl);
+            for (const sub_key in tmp_obj) {//選択肢のループ
+                if (tmp_obj.hasOwnProperty(sub_key)) {
+                    const sub_element = tmp_obj[sub_key];
+                    console.log(sub_key +' : ' +sub_element);
+
+                    //TODO::inputラジオボタンを選択肢としてdivに追加
+                    //inputとlabelがセットになって追加する
+
+
+                }
+            }
+
+        }
+    }
+
+    liParEl.appendChild(divEl);
+    form.appendChild(liParEl);
+
+//     var tmpLabelEl = document.createElement('label');
+//     tmpLabelEl.textContent = 'わろたｗｗ'
+
+//     var tmpInputEl = document.createElement('input');
+//     tmpInputEl.type = 'radio';
+//     tmpInputEl.name = 'mental';
+//     tmpInputEl.form='mainform';
+// tmpInputEl.value='てすとｗ'
+
+//     form.appendChild(tmpLabelEl);
+//     form.appendChild(tmpInputEl);
 }
