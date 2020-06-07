6a0a4e155e57a1be1e798a994b43504ab1c5a006
ruto-txt <48617610+ruto-txt@users.noreply.github.com>
2020-06-07 11:45:46 +0900

    ラジオボタンのレイアウトを、bootstrapのグリッドレイアウトが適用できるように変更
    ついでに、「ラジオボタンを追加する関数」でなんか暗号めいていた変数をちょっと書き換えた。
    未入力のラジオオボタンを通知する機能をつけようとしたけど、やめました。ラジオボタンを変更するたびにリアルタイムでツイート文面に反映されるタイプのツイートボタンがあるから、いらないかもと思った。

--- vreco-project/public/script.js
+++ vreco-project/public/script.js
@@ -10,47 +10,56 @@ function inputsInit(choices,qSentence){
             const tmp_obj = choices[key];
             
             // 設問ブロックを包むdivを生成
-            var liParEl = document.createElement('li');//番号つきリストに加える要素を生成
-            var divEl = document.createElement('div');
+            var li = document.createElement('li');//番号つきリストに加える要素を生成
+            li.id = key;//liアイテムにアクセスするためのidは,formのnameプロパティと同じものにしてみよう
+            var parentDivEl = document.createElement('div');
             
             // TODO:elementにIDを大問名で追加する。後のエラーチェックでクラスを追加するのに使おうと思っている
             
-            divEl.className = 'form-group';
+            parentDivEl.className = 'form-group';
         
-            var tmpEl = document.createElement('p');
-            tmpEl.textContent = qSentence[key];//設計ルールで設問文は同じkeyを使うようになっている
-            divEl.appendChild(tmpEl);
-            liParEl.appendChild(divEl);
-            ol.appendChild(liParEl);
+            var questionSentenceEl = document.createElement('p');
+            questionSentenceEl.textContent = qSentence[key];//設計ルールで設問文は同じkeyを使うようになっている
+            parentDivEl.appendChild(questionSentenceEl);
+            li.appendChild(parentDivEl);
+            ol.appendChild(li);
 
             let i = 1;
+            var rowDiv = document.createElement('div');
+            rowDiv.className='form-row';
             for (const sub_key in tmp_obj) {//選択肢のループ
                 if (tmp_obj.hasOwnProperty(sub_key)) {
+                    let colDiv =document.createElement('div');
+                    colDiv.className='col-sm-2';
+                    
                     const sub_element = tmp_obj[sub_key];
                     var inputEl = document.createElement('input');
-                    // inputEl.className = 'form-check-input'　//TODO::CSSクラスを追記
-                    //もしかしたら、しないほうがいいかも
                     inputEl.type = 'radio';
                     inputEl.id=sub_key;
                     inputEl.name = key;//name属性には親要素と同じ内容を書き込む
                     inputEl.value = sub_element;
 
                     var inputlb = document.createElement('label');
-                    // inputlb.className="form-check-label" //TODO::CSSクラスを追記
-                    inputlb.for = sub_key;
+                    inputlb.htmlFor = sub_key;
                     inputlb.textContent = sub_element;
 
-
-                    divEl.appendChild(inputEl);
-                    divEl.appendChild(inputlb);
+                    /* グリッドlayout用のDivに収めてからの格納に変更
+                    parentDivEl.appendChild(inputEl);
+                    parentDivEl.appendChild(inputlb);
+                    */
+                   colDiv.appendChild(inputEl);
+                   colDiv.appendChild(inputlb);
+                   rowDiv.appendChild(colDiv);
                     if (i%3==0) {
-                        divEl.appendChild(document.createElement('br'));
-                        // 三の倍数でbr要素を追加
+                        // 三の倍数でrowクラスを持つdiv要素を追加
+                        parentDivEl.appendChild(rowDiv);
+                        var rowDiv = document.createElement('div');
+                        rowDiv.className='form-row';
                         i=0
                     };
                     i++;
 
-                    ol.insertBefore(liParEl,document.getElementById('url-adrress'));
+                    ol.insertBefore(li,document.getElementById('url-adrress'));
                     //追加内容を、input(type=url)要素と入れ替える。urlが最下部になるように。
                 }
             }
@@ -84,19 +93,10 @@ function acquireForm(){
                 exist=true;
             }
         });
-
-        /*
-        //TODO：なにもヒットしなかった場合、エラーを返したい
-        //エラーがある座標すべてに対してエラーを表現するcssクラスを追加することによって
-        if (!exist) {
-            //要素を同定し、適した範囲にcssクラスを追加する
-        }
-        */
     }
 
-
-    //TODO:anserArrをメソッドに投げて、tweetに使う文字列に変換したい。。。
-
+    //TODO:anserArrをメソッドに投げて、tweetに使う文字列に変換する。
+    //変換後の文字をreturnする
 
     var anserObj={name:vname,url:url,text:anserArr};
     return anserObj;
@@ -121,7 +121,7 @@ function henkan(src){
 
 
 /*
-*　文面をtweetタグへ変換し、tweetボタンを生成・上書きするメソッド
+*　tweetボタンを生成・上書きするメソッド
 *　tweetボタンにぶち込む文面を引数に入れてメソッドを呼び出す
 *　参考url = https://qiita.com/lovesaemi/items/d4f296b6b1d5158d2fea
 // 任意のタイミングで呼べば狙ったとおりのテキストのボタンつくれる
@@ -159,6 +159,10 @@ function setTweetButton(anserObj){
 //参考url http://westplain.sakuraweb.com/translate/twitter/Documentation/Twitter-for-Websites/Tweet-Button/JavaScript-Factory-Function.cgi
 // http://westplain.sakuraweb.com/translate/twitter/Documentation/Twitter-for-Websites/Tweet-Button/Parameter-Reference.cgi
 
+
+/*
+* ラジオボタンの状態が変わるたびに呼び出され、httpqueryの内容を上書きする
+*/
 function updateTweetbutton(){
     const anserObj = acquireForm();
     var tmp = document.getElementById('tweetbutton')
@@ -170,6 +174,7 @@ function updateTweetbutton(){
 }
 
 
+
 /*
 *　読み込み完了時にメソッド点火
 *　choices,questionSentenceはquestion.jsで宣言したオブジェクト
@@ -178,4 +183,3 @@ inputsInit(choices,questionSentence);
 
 //formを操作するたびにtweetのソースを上書きするイベントリスナ追加
 document.forms[0].addEventListener("change",updateTweetbutton)
-
